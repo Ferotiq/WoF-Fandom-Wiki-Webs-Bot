e8ea9380218dd263a4d895f80c9264cd666beb0a
Ferotiq <64989416+Ferotiq@users.noreply.github.com>
2020-08-05 21:10:46 -0500

    Fix moderation.js
    Command should only be able to be run by admins but was mistakenly allowing custom role mods to use it

--- Commands/moderation.js
+++ Commands/moderation.js
@@ -10,7 +10,7 @@ module.exports = {
     execute(message, args, modRole, onlyChannel, prefix) {
 
         // Makes sure the person issuing the command has the admin permission or the modRole
-        if (!message.member.hasPermission('ADMINISTRATOR') && !message.member.roles.cache.get(modRole)) return message.channel.send('Permission\'t');
+        if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Permission\'t');
 
         // Makes sure there is a second argument
         if (!args[1]) return message.channel.send('You didn\'t provide a second argument! (Arguments: set, view)');
